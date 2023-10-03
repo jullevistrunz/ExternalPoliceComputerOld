efcd818199ae75c5b7a57c15c820a761c3094d32
timnboys <timnboys@users.noreply.github.com>
2023-10-02 17:48:14 -0700

    Update Main.cs
    Added GameFiber.Yield();
    which is required to yield execution back to the game & other RagePluginHook Plugins to avoid game freezing/crashing.
    See Here for more details:
    https://docs.ragepluginhook.net/html/4c04eafe-2474-4f41-9087-ac47bb16d491.htm

--- Plugin with Timer (will crash GTA)/Main.cs
+++ Plugin with Timer (will crash GTA)/Main.cs
@@ -43,52 +43,66 @@ namespace ExternalPoliceComputer {
         }
 
         private static void TimerCars_Elapsed(object sender, ElapsedEventArgs e) {
+            GameFiber.Yield();
             updateWorldCars();
+            GameFiber.Yield();
         }
 
         private static void TempTimerCars_Elapsed(object sender, ElapsedEventArgs e) {
+            GameFiber.Yield();
             tempTimerCars.Enabled = false;
             timerCars.Enabled = true;
+            GameFiber.Yield();
         }
 
         private static void TimerPeds_Elapsed(object sender, ElapsedEventArgs e) {
+            GameFiber.Yield();
             updateWorldPeds();
+            GameFiber.Yield();
         }
 
        
         private static void updateWorldPeds() {
+            GameFiber.Yield();
             Game.LogTrivial("ExternalPoliceComputer: Update EPC/worldPeds.data");
             Ped[] allPeds = World.GetAllPeds();
+            GameFiber.Yield();
             string[] persList = new string[allPeds.Length];
-           
+            GameFiber.Yield();
             foreach (Ped ped in allPeds) {
+               GameFiber.Yield();
                if (ped.Exists()) {
+                   GameFiber.Yield();
                    persList[Array.IndexOf(allPeds, ped)] = $"name={Functions.GetPersonaForPed(ped).FullName}&birthday={Functions.GetPersonaForPed(ped).Birthday.Month}/{Functions.GetPersonaForPed(ped).Birthday.Day}/{Functions.GetPersonaForPed(ped).Birthday.Year}&gender={Functions.GetPersonaForPed(ped).Gender}&isWanted={Functions.GetPersonaForPed(ped).Wanted}&licenseStatus={Functions.GetPersonaForPed(ped).ELicenseState}";
                }
-
             }
-
+            GameFiber.Yield();
             File.WriteAllText("EPC/worldPeds.data", string.Join(",", persList));
-
+            GameFiber.Yield();
             Game.LogTrivial("ExternalPoliceComputer: Updated EPC/worldPeds.data");
         }
 
 
         private static void updateWorldCars() {
+            GameFiber.Yield();
             Game.LogTrivial("ExternalPoliceComputer: Update EPC/worldCars.data");
             Vehicle[] allCars = World.GetAllVehicles();
+            GameFiber.Yield();
             string[] carsList = new string[allCars.Length];
             string driver;
-            
+            GameFiber.Yield();
             foreach (Vehicle car in allCars) {
+                GameFiber.Yield();
                 if (car.Exists()) {
+                    GameFiber.Yield();
                     driver = car.Driver.Exists() ? Functions.GetPersonaForPed(car.Driver).FullName : "";
                     carsList[Array.IndexOf(allCars, car)] = $"licensePlate={car.LicensePlate}&model={car.Model.Name}&isStolen={car.IsStolen}&isPolice={car.IsPoliceVehicle}&driver={driver}";
                 }
+                GameFiber.Yield();
             }
-            
+            GameFiber.Yield();
             File.WriteAllText("EPC/worldCars.data", string.Join(",", carsList));
-
+            GameFiber.Yield();
             Game.LogTrivial("ExternalPoliceComputer: Updated EPC/worldCars.data");
         }
 
